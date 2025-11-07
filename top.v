59b58e88b34a8d0ef8233468b2e68133db6b62df
sarahxpearce <sarahpearce@u.boisestate.edu>
2025-11-07 00:34:58 -0700

    Update top.v

--- top.v
+++ top.v
@@ -1,6 +1,6 @@
 module top(
-    input  [15:0] sw,
-    input         btnL, btnU, btnD, btnR, btnC,
+    input [15:0] sw,
+    input btnL, btnU, btnD, btnR, btnC,
     output [15:0] led
 );
     wire [1:0] ms = {btnU, btnL};
