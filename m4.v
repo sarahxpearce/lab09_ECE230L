316213149737de2e0b21881942fba7691df7c4ad
sarahxpearce <sarahpearce@u.boisestate.edu>
2025-11-07 00:35:27 -0700

    Update m4.v

--- m4.v
+++ m4.v
@@ -1,7 +1,7 @@
 module m4(
-    input  [3:0] a, b, c, d,
-    input  [1:0] s,
-    input        en,
+    input [3:0] a, b, c, d,
+    input [1:0] s,
+    input en,
     output [3:0] y
 );
     assign y = en ? (s==2'b00 ? a : s==2'b01 ? b : s==2'b10 ? c : d) : 4'b0000;
