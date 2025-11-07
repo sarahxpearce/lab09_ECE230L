e740e7172991aadc0e7a54afabe97a3711ceb9a5
sarahxpearce <sarahpearce@u.boisestate.edu>
2025-11-07 00:35:49 -0700

    Update d4.v

--- d4.v
+++ d4.v
@@ -1,7 +1,7 @@
 module d4(
-    input  [3:0] x,
-    input  [1:0] s,
-    input        en,
+    input [3:0] x,
+    input [1:0] s,
+    input en,
     output [3:0] y0, y1, y2, y3
 );
     assign y0 = en ? (s==2'b00 ? x : 4'b0000) : 4'b0000;
